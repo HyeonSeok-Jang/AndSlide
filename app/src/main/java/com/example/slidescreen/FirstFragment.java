75e0296817552c2162423191d9e16f56e5a0c17d
HyeonSeok-Jang <dkdidudtn@gmail.com>
2021-03-27 15:53:18 +0900

    slide view and change

---
+++ app/src/main/java/com/example/slidescreen/FirstFragment.java
@@ -0,0 +1,29 @@
+package com.example.slidescreen;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FirstFragment extends Fragment {
+    public FirstFragment(){
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.fragment1,container,false);
+        return layout;
+    }
+}
